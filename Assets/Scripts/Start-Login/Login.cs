d41751e4cf0f4b8ba5b17fec66d1450491a35b04
Isaac Yang <isaacylr96@hotmail.com>
2020-03-04 13:16:31 +0800

    Init Home Screen UI

---
+++ Assets/Scripts/Start-Login/Login.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine.UI;
+using UnityEngine;
+using TMPro;
+
+public class Login : MonoBehaviour
+{
+    public Button LoginButton;
+    public CanvasGroup LoginUICanvas;
+    public TMP_InputField Username;
+    public TMP_InputField Password;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        LoginButton.onClick.AddListener(LogIn);
+    }
+
+    private void LogIn()
+    {
+        Debug.Log("Login Clicked!");
+
+        Debug.Log("Username: " + Username.text);
+
+        Debug.Log("Password: " + Password.text);
+
+    }
+}
